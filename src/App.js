a728bef8c2cf27d43db71d7a7e5fa232965745bd
Rodrigo Viana <rodrigoviana_1@hotmail.com>
2022-08-15 21:32:54 -0300

    mensagem

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { Bstrash, BsBookmarkcheck, BsBookmarkcheckFill } from 'react-icons/bs'
 import axios from 'axios'
 import { format } from 'date-fns';
-
+import { confirm } from 'react-confirm-box'
 
 
 // import {confirm} from 'react-confirm'
@@ -14,119 +14,69 @@ function App() {
 
   const [cracha, setCracha] = useState("");
   var today = format(new Date(), 'dd/MM/yyyy');
-
+  var numeroCrachaEncontrado
+  var nomeObreiro
+  let numcrachaResgister = false;
+  let numcrachaIsExist = false;
   const handleSubmit = async (e) => {
+    let flag = false;
     e.preventDefault()
-    let flaq = false;
 
     const opt = {
       method: 'GET',
       mode: 'cors',
       cache: 'default'
     }
-    fetch(API + '/obreiros', opt)
+
+    // Aqui pesquisando tabela CHAMADA se o cracha já foi registrado
+    await fetch(API + '/chamada', opt)
       .then((r) => r.json())
       .then(resposta => {
-        console.log(resposta)
+        resposta.map((num) => {
+          console.log(typeof num.cracha + " ------------------ " + typeof cracha)
+          console.log(num.cracha + " > " + parseInt(cracha))
+          if (parseInt(cracha) === num.cracha) {
+            numcrachaResgister = true
+            console.log('enttrou aqui')
+          }
+        }
+        )
+        if (numcrachaResgister) {
+          swal.fire("Opa!", "Número do Crachá já registrado !!", "warning")
+        }
+      });
+
+    // Aqui pesquisando tabela OBREIROS se o cracha existe
+    await fetch(API + '/obreiros', opt)
+      .then((r) => r.json())
+      .then(resposta => {
+        let numcrachaIsExist = false;
         resposta.map((num) => {
           console.log(typeof num.cracha + " <<>> " + typeof cracha)
           console.log(num.cracha + " > " + parseInt(cracha))
           if (parseInt(cracha) === num.cracha) {
-            flaq = true
+            numcrachaIsExist = true
+            numeroCrachaEncontrado = num.cracha;
+            nomeObreiro = num.nome;
           }
         }
         )
-        if (!flaq) {
+        if (!numcrachaIsExist) {
           swal.fire("Opa!", "Número do Crachá não existe !!", "warning")
         }
+
       });
-    // axios.request(teste).then((response) => { console.log(response.data) })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    const respObr = await axios.get(`http://localhost:3001/obreiros`)
-
-    const resp = await axios.get(API + "/cracha")
-
-    // respObr.data.map((obrExiste) => {
-    //   breakme: {
-    //     if(parseInt(cracha) !== obrExiste.cracha){
-    //       console.log('Data do banco de dados => ' + obrExiste)
-    //       flaq = false;
-    //       // alert("Crachá já cadastrado")
-    //       swal.fire("Opa!", "Número do Crachá não existe !!", "warning")
-    //       break breakme;
+    // const a = async () => {
+    //   if (numcrachaIsExist === true) {
+    //     if (window.confirm("Número " + numeroCrachaEncontrado + ": " + nomeObreiro + "?")) {
+    //       await axios.post(API + "/chamada", { "cracha": parseInt(cracha) })
     //     }
     //   }
-    // })
-
-    resp.data.map((dados) => {
-
-      // Data do banco pegando para comparar com a data de hoje
-      var dataDB = format(new Date(dados.dataCriacao), 'dd/MM/yyyy')
+    // }
 
 
-      breakme: {
-        if (parseInt(cracha) === dados.cracha && today === dataDB) {
-          flaq = false;
-          // alert("Crachá já cadastrado")
-          swal.fire("Opa!", "Número do Crachá já inserido !!!", "warning")
-          break breakme;
-        }
-      }
-    })
-    if (flaq) {
-
-      respObr.data.map((obr) => {
-        breakme: {
-          if (parseInt(cracha) === obr.cracha) {
-            // let nm =  swal.fire("teste",{buttons:[true,'false']});
-
-            const nm = swal.fire({
-              title: "Número " + obr.cracha + ": " + obr.nome + "?",
-              text: "",
-              icon: "warning",
-              showCancelButton: true,
-              confirmButtonColor: '#3085d6',
-              cancelButtonColor: '#3085d6',
-              confirmButtonText: 'Confirma'
-            }).then((resultado) => {
-              if (resultado.isConfirmed) {
-                swal.fire({
-                  icon: 'success',
-                  title: 'Inserindo com sucesso!',
-                  showConfirmButton: false,
-                  timer: 1500
-                })
-              }
-            })
-
-            // window.confirm(obr.nome);
-            flaq = nm ? true : false
-            break breakme;
-          }
-        }
-      })
-
-
-      if (flaq) {
-        await axios.post(API + "/cracha", { "cracha": parseInt(cracha) })
-      }
-    }
+    // }
 
     setCracha("");
 
@@ -142,7 +92,7 @@ function App() {
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <input
-              type="text"
+              type="number"
               onKeyPress={e => { if (!/[0-9]/.test(e.key)) e.preventDefault() }}
               name="cracha"
               placeholder="Numero da chamada"
