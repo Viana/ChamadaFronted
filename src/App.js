c567abaa41ad268382c20268b61d151c9b70d8f0
Rodrigo Viana <rodrigoviana_1@hotmail.com>
2022-08-17 19:07:11 -0300

    validação no back

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { format } from 'date-fns';
 import { confirm } from 'react-confirm-box'
 
 
+
 // import {confirm} from 'react-confirm'
 const API = 'http://localhost:3001';
 
@@ -14,69 +15,24 @@ function App() {
 
   const [cracha, setCracha] = useState("");
   var today = format(new Date(), 'dd/MM/yyyy');
-  var numeroCrachaEncontrado
-  var nomeObreiro
-  let numcrachaResgister = false;
-  let numcrachaIsExist = false;
+
   const handleSubmit = async (e) => {
     let flag = false;
     e.preventDefault()
 
-    const opt = {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'default'
+    try {
+      let nomeObr;
+      await axios.get(API + `/obreiros/cracha/${cracha}`)
+        .then(re => {
+
+          re.data.map((r) => {
+            console.log(r.nome)
+          })
+        })
+
+    } catch (error) {
+      console.log(error.response.data.Msg)
     }
-
-    // Aqui pesquisando tabela CHAMADA se o cracha já foi registrado
-    await fetch(API + '/chamada', opt)
-      .then((r) => r.json())
-      .then(resposta => {
-        resposta.map((num) => {
-          console.log(typeof num.cracha + " ------------------ " + typeof cracha)
-          console.log(num.cracha + " > " + parseInt(cracha))
-          if (parseInt(cracha) === num.cracha) {
-            numcrachaResgister = true
-            console.log('enttrou aqui')
-          }
-        }
-        )
-        if (numcrachaResgister) {
-          swal.fire("Opa!", "Número do Crachá já registrado !!", "warning")
-        }
-      });
-
-    // Aqui pesquisando tabela OBREIROS se o cracha existe
-    await fetch(API + '/obreiros', opt)
-      .then((r) => r.json())
-      .then(resposta => {
-        let numcrachaIsExist = false;
-        resposta.map((num) => {
-          console.log(typeof num.cracha + " <<>> " + typeof cracha)
-          console.log(num.cracha + " > " + parseInt(cracha))
-          if (parseInt(cracha) === num.cracha) {
-            numcrachaIsExist = true
-            numeroCrachaEncontrado = num.cracha;
-            nomeObreiro = num.nome;
-          }
-        }
-        )
-        if (!numcrachaIsExist) {
-          swal.fire("Opa!", "Número do Crachá não existe !!", "warning")
-        }
-
-      });
-
-    // const a = async () => {
-    //   if (numcrachaIsExist === true) {
-    //     if (window.confirm("Número " + numeroCrachaEncontrado + ": " + nomeObreiro + "?")) {
-    //       await axios.post(API + "/chamada", { "cracha": parseInt(cracha) })
-    //     }
-    //   }
-    // }
-
-
-    // }
 
     setCracha("");
 
